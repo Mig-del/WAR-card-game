93cd363f617cf5b14ece3736c3d26d86b0e8b7c9
username <youremail@domain.com>
2022-03-22 22:40:26 -0700

    card logic working

--- js/script.js
+++ js/script.js
@@ -1,117 +1,106 @@
-console.log('it works');
+console.log("it works");
 
-
-
-values = ['13', '12', '11', '10', '9', '8', '7', '6', '5','4', '3','2', '1']
-suits = ['spades', 'hearts', 'diamonds', 'clubs']
-
-
-
+values = ["13", "12", "11", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"];
+suits = ["spades", "hearts", "diamonds", "clubs"];
 
 const cards = [];
 for (let s = 0; s < suits.length; s++) {
-    for (let v = 1; v < 14; v++) {
+  for (let v = 1; v < 14; v++) {
     //     const value = values[v];
-        const suit = suits[s];
-        cards.push({v, suit});
-        
-    }
+    const suit = suits[s];
+    cards.push({ v, suit });
+  }
 }
 console.log(cards);
 
-
 function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-  }
+  array.sort(() => Math.random() - 0.5);
 
-  shuffle(cards);
+  
+}
+shuffle(cards);
 
   console.log(cards);
 
+  let playerOneDeck = cards.slice(0, 26);
+  let playerTwoDeck = cards.slice(26);
 
+  console.log(playerOneDeck);
+  console.log(playerTwoDeck);
 
-let playerOneDeck = cards.slice(0, 26);
-let playerTwoDeck = cards.slice(26);
+  // making cards to be random when start of the game
+  // 26 cards each player
 
+  const random = Math.floor(Math.random() * 25);
+  const playerOneValue = playerOneDeck[0].v;
+  const playerOneSuit = playerOneDeck[0].suit;
 
+  const playerTwoValue = playerTwoDeck[0].v;
+  const playerTwoSuit = playerTwoDeck[0].suit;
+// console.log(playerOneValue);
+// console.log(playerTwoValue);
+  document.body.onload = addElement;
 
-console.log(playerOneDeck);
-console.log(playerTwoDeck);
+  function addElement() {
+    // create a new div element for player 1
+    const newDiv = document.createElement("div");
+    newDiv.id = "playerOne";
 
+    const playerOneCard = document.createElement("img");
+    playerOneCard.setAttribute(
+      "src",
+      "./assets/images/" + playerOneSuit + "-" + playerOneValue + ".svg"
+    );
 
+    newDiv.appendChild(playerOneCard);
 
-// making cards to be random when start of the game
-// 26 cards each player
-
-const random = Math.floor(Math.random() * 25);
-const playerOneValue = playerOneDeck[random].v;
-const playerOneSuit = playerOneDeck[random].suit;
-
-const playerTwoValue = playerTwoDeck[random].v;
-const playerTwoSuit = playerTwoDeck[random].suit;
-
-
-
-
-
-
-document.body.onload = addElement;
-
-
-
-
-
-function addElement () {
-  // create a new div element for player 1
-  const newDiv = document.createElement("div");
-  newDiv.id = "playerOne";
-
-  
-  const newContent = document.createElement("img");
-    newContent.setAttribute("src", "./assets/images/" + playerOneSuit + "-" + playerOneValue + ".svg")  
-
-  
-  newDiv.appendChild(newContent);
-
-
-  
-  const currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
 
     // create a new div element for player 2
     const div2 = document.createElement("div");
-  
-    
-    const content2 = document.createElement("img");
-      content2.setAttribute("src", "./assets/images/" + playerTwoSuit + "-" + playerTwoValue + ".svg")  
-  
-    div2.appendChild(content2);
+    div2.id = "playerTwo";
+
+    const playerTwoCard = document.createElement("img");
+    playerTwoCard.setAttribute(
+      "src",
+      "./assets/images/" + playerTwoSuit + "-" + playerTwoValue + ".svg"
+    );
+
+    div2.appendChild(playerTwoCard);
 
     const currentDiv2 = document.getElementById("div1");
     document.body.insertBefore(div2, currentDiv2);
+  }
 
-}
+  playerOneCard = playerOneDeck.shift();
+
+  playerTwoCard = playerTwoDeck.shift();
+  cardsBeingPlayed = [];
+
+console.log(playerOneCard);
+console.log(playerTwoCard);
+
+//   while ((playerOneDeck.length > 0, playerTwoDeck.length > 0)) {
+    if (playerOneCard.v > playerTwoCard.v) {
+        console.log('playerOne wins');
+      cardsBeingPlayed.push(playerOneCard, playerTwoCard);
+      playerOneDeck.push(cardsBeingPlayed);
+    } else if (playerTwoCard.v > playerOneCard.v) {
+        console.log('playertwo wins')
+      cardsBeingPlayed.push(playerOneCard, playerTwoCard);
+      playerTwoDeck.push(cardsBeingPlayed);
+    } else if (playerOneCard.v === playerTwoCard.v) {
+        console.log('tie')
+      playerOneDeck.shift(cardsBeingPlayed);
+      playerTwoDeck.shift(cardsBeingPlayed);
+      playerOneCard.v > playerTwoCard.v;
+      cardsBeingPlayed.push(playerOneCard, playerTwoCard);
+      playerOneDeck.push(cardsBeingPlayed);
+    } 
+//   }
 
 
-let keepPlaying = function(winningCards){
-    if (winningCards) {console.log('winningCards', winningCards);}
-}
-
-// tie
-if (playerOneValue === playerTwoValue){
-    console('tie')
-
-    // play another card
-    winningCard.push(playerOneDeck);
-    winningCard.push(playerTwoDeck);
-    
-} else if (playerOneValue > playerTwoValue){
-    console.log('Player One wins draw');
-
-    playerOneDeck.push(playerOneDeck.length, 0)
-
-
-}
-
-
-
+let button = function onClick() {
+  document.getElementById("draw").click();
+};
