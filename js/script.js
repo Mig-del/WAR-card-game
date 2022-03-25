1ca8e12a05dd84635d827d188f013bbba86201b1
username <youremail@domain.com>
2022-03-24 18:00:21 -0700

    finished css js html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log("it works");
+
 
 values = ["13", "12", "11", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"];
 suits = ["spades", "hearts", "diamonds", "clubs"];
@@ -11,20 +11,18 @@ for (let s = 0; s < suits.length; s++) {
     cards.push({ v, suit });
   }
 }
-console.log(cards);
+
 
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
 }
 shuffle(cards);
 
-console.log(cards);
+
 
 let playerOneDeck = cards.slice(0, 26);
 let playerTwoDeck = cards.slice(26);
 
-console.log(playerOneDeck);
-console.log(playerTwoDeck);
 
 // making cards to be random when start of the game
 // 26 cards each player
@@ -39,37 +37,37 @@ let playerTwoSuit = playerTwoDeck[0].suit;
 document.body.onload = addElement;
 
 function addElement() {
-  // create a new div element for player 1
+  
   const newDiv = document.createElement("div");
   newDiv.id = "playerOne";
 
-  const playerOneCard = document.createElement("img");
-  playerOneCard.setAttribute(
+  const playerOneCardImg = document.createElement("img");
+  playerOneCardImg.setAttribute(
     "src",
     "./assets/images/" + playerOneSuit + "-" + playerOneValue + ".svg"
   );
-  playerOneCard.id = "playerOneCard";
-
-  newDiv.appendChild(playerOneCard);
+  playerOneCardImg.id = "playerOneCardImg";
+  
+  newDiv.appendChild(playerOneCardImg);
 
   const currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
+  document.getElementById('playingCardsDiv').appendChild(newDiv);
 
   // create a new div element for player 2
   const div2 = document.createElement("div");
   div2.id = "playerTwo";
 
-  const playerTwoCard = document.createElement("img");
-  playerTwoCard.setAttribute(
+  const playerTwoCardImg = document.createElement("img");
+  playerTwoCardImg.setAttribute(
     "src",
     "./assets/images/" + playerTwoSuit + "-" + playerTwoValue + ".svg"
   );
-  playerTwoCard.id = "playerTwoCard";
+  playerTwoCardImg.id = "playerTwoCardImg";
 
-  div2.appendChild(playerTwoCard);
+  div2.appendChild(playerTwoCardImg);
 
   const currentDiv2 = document.getElementById("div1");
-  document.body.insertBefore(div2, currentDiv2);
+  document.getElementById('playingCardsDiv').appendChild(div2);
 }
 
 let playerOneCard = playerOneDeck.shift();
@@ -82,42 +80,35 @@ console.log(playerTwoCard);
 
 function gameLogic() {
   if (playerOneCard.v > playerTwoCard.v) {
+   document.getElementById('winner').innerHTML='Player One Win Round!';
     console.log("playerOne wins");
     if (tieCards.length > 0) {
       tieCards.forEach(card => playerOneDeck.push(card));
       tieCards = [];
     }
     playerOneDeck.push(playerOneCard, playerTwoCard);
-    // playerOneDeck.push(cardsBeingPlayed);
   } else if (playerTwoCard.v > playerOneCard.v) {
+    document.getElementById('winner').innerHTML ='Player Two Win Round!';
     if (tieCards.length > 0) {
       tieCards.forEach(card => playerTwoDeck.push(card));
       tieCards = [];
     }
     console.log("playertwo wins");
     playerTwoDeck.push(playerOneCard, playerTwoCard);
-    // playerTwoDeck.push(cardsBeingPlayed);
   } else if (playerOneCard.v === playerTwoCard.v) {
+    document.getElementById('winner').innerHtml='Tie! Draw Again!';
     console.log("tie");
     tieCards.push(playerOneCard, playerTwoCard);
-
-      // if (tieCards > 0){
-      //   forEach()
-      // }
-
-
-    // playerOneDeck.push(playerOneCard);
-    // playerTwoDeck.push(playerTwoCard);
-    // playerOneDeck.shift(cardsBeingPlayed);
-    // playerTwoDeck.shift(cardsBeingPlayed);
     
-    // cardsBeingPlayed.push(playerOneCard, playerTwoCard);
-//     playerOneDeck.push(cardsBeingPlayed);
-      }
+  }console.log(playerOneDeck);
+  console.log(playerTwoDeck);
+  console.log(tieCards)
 }
+
 gameLogic();
 
 function draw() {
+  // while (playerOneDeck.length > 0 || playerTwoDeck.length > 0) {
   if (playerOneDeck.length == 0 || playerTwoDeck.length == 0) {
     console.log("game over");
   } else {
@@ -126,8 +117,8 @@ function draw() {
 
     playerTwoValue = playerTwoDeck[0].v;
     playerTwoSuit = playerTwoDeck[0].suit;
-    let playerOneCardImg = document.getElementById("playerOneCard");
-    let playerTwoCardImg = document.getElementById("playerTwoCard");
+    let playerOneCardImg = document.getElementById("playerOneCardImg");
+    let playerTwoCardImg = document.getElementById("playerTwoCardImg");
     playerOneCardImg.setAttribute(
       "src",
       "./assets/images/" + playerOneSuit + "-" + playerOneValue + ".svg"
@@ -139,7 +130,10 @@ function draw() {
     playerOneCard = playerOneDeck.shift();
     playerTwoCard = playerTwoDeck.shift();
     gameLogic();
-  } 
-  console.log(playerOneDeck)
-  console.log(playerTwoDeck)
+    document.getElementById('score').innerHTML= `player one: ${playerOneDeck.length} | player two ${playerTwoDeck.length} `
+    
+  }
+//  // console.log(playerOneDeck);
+//   // console.log(playerTwoDeck);
 }
+
